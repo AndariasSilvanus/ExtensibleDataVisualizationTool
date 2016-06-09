91c01e3a064b7459fe8e9c84a67e909b6549f03d
Andarias <asilvanus5@gmail.com>
2016-06-09 23:55:04 +0700

    code cleaning

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -15,16 +15,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 require APPPATH.'/libraries/REST_Controller.php';
 
 class Api extends REST_Controller {
-    // specify own database credentials
-    private $host = "localhost";
-    private $db_name = "extensibleVisualization";
-    private $username = "root";
-    private $password = "";
-
-    // attribute
-    private $columnName = array();
-    private $conn;
-    private $data;
 
     // constructor
     function __construct() {
@@ -39,26 +29,27 @@ class Api extends REST_Controller {
 
 	public function addData_post(){
 
-		$this->data = json_decode(file_get_contents("php://input"), true);
-        if (!empty($this->data) && $this->data && isset($this->data)) {
+//		$this->data = json_decode(file_get_contents("php://input"), true);
+        $this->api_model->setData(json_decode(file_get_contents("php://input"), true));
+        if (!empty($this->api_model->getData()) && $this->api_model->getData()) {
 
             if (!($_SESSION["first"])) {
-                if ($this->invokeCreateTable()) {
+                if ($this->api_model->invokeCreateTable()) {
                     $_SESSION["first"] = true;
                     echo "\ntableName: ".$_SESSION["tableName"];
                     echo "\ntable created";
-                    $this->insertData();
+                    $this->api_model->insertData();
                 }
             }
             else {
-                if ($this->deleteTable($_SESSION["tableName"])) {
+                if ($this->api_model->deleteTable($_SESSION["tableName"])) {
                     echo "\ntable deleted";
                     $_SESSION["tableName"] = "";
                     $this->columnName = empty($this->columnName);
-                    if ($this->invokeCreateTable()) {
+                    if ($this->api_model->invokeCreateTable()) {
                         echo "\ntableName: ".$_SESSION["tableName"];
                         echo "\ntable created";
-                        $this->insertData();
+                        $this->api_model->insertData();
                     }
                 }
             }
@@ -83,8 +74,8 @@ class Api extends REST_Controller {
 
     public function deleteDimension_get() {
         $dimensionName = $this->get('dimensionName');
-        if (dimensionName){
-            if (($key_idx = array_search(dimensionName, $_SESSION["measure"])) !== false) {
+        if ($dimensionName){
+            if (($key_idx = array_search($dimensionName, $_SESSION["measure"])) !== false) {
                 unset($_SESSION["dimension"][$key_idx]);
             }
             $this->response($_SESSION["dimension"], 200);
@@ -93,7 +84,7 @@ class Api extends REST_Controller {
 
     public function pushDimension_get() {
         $dimensionName = $this->get('dimensionName');
-        if (dimensionName){
+        if ($dimensionName){
             array_push($_SESSION["dimension"], $dimensionName);
             $this->response($_SESSION["dimension"], 200);
         }
@@ -137,159 +128,6 @@ class Api extends REST_Controller {
             }
             $this->response($_SESSION["measure_type"], 200);
         }
-    }
-
-    // Controller function
-    private function connect(){
-        $this->conn = null;
-
-        $this->conn = mysqli_connect($this->host, $this->username, $this->password, $this->db_name);
-        if (mysqli_connect_errno()){
-            // can't connect to mySQL
-        }
-
-        return $this->conn;
-    }
-
-    private function destroyConn(){
-        mysqli_close($this->conn);
-    }
-
-    private function invokeCreateTable() {
-        $_SESSION["tableName"] = $this->generateString(50);
-        $this->columnName = array_keys($this->data[0]);
-        $dataExample = $this->data[0];
-        $dataExample = array_values($dataExample);
-        return ($this->createTable($_SESSION["tableName"], $this->columnName, $dataExample));
-    }
-
-    private function generateString($length){
-        $charset = "abcdefghijklmnopqrstuvwxyz0123456789";
-        $key = "";
-        for($i=0; $i<$length; $i++)
-            $key .= $charset[(mt_rand(0,(strlen($charset)-1)))];
-        return $key;
-    }
-
-    private function createTable($tableName, $columnName, $dataExample) {
-
-        if ($this->connect()) {
-            // Build mySQL query to create table
-            $query = 'CREATE TABLE IF NOT EXISTS `' . $tableName . '`(';
-            $numItems = count($columnName);
-            $i = 0;
-
-            // Dimension/Measure session preparation
-            $dimensionArr = array();
-            $measureArr = array();
-            $measureTypeArr = array();
-
-            foreach ($columnName as $col) {
-                if (ctype_digit($dataExample[$i])) {
-                    $query = $query . '`' . $col . '` INT NOT NULL';
-                    array_push($measureArr, $col);
-                    array_push($measureTypeArr, "SUM");
-                }
-                else {
-                    $query = $query . '`' . $col . '` TEXT NOT NULL';
-                    array_push($dimensionArr, $col);
-                }
-
-                $i++;
-                if ($i != $numItems) // not last index
-                    $query = $query . ',';
-                else
-                    $query = $query . ')';
-            }
-
-            // Push column name to Dimension/Measure session
-            $_SESSION["dimension"] = $dimensionArr;
-            $_SESSION["measure"] = $measureArr;
-
-            // Insert query to database
-            $result = mysqli_query($this->conn, $query);
-            echo "\nQUERY: ".$query;
-            echo "\nResult: ".$result;
-            $this->destroyConn();
-
-            if ($result) return TRUE;
-            else return FALSE;
-        }
-        else return FALSE;
-    }
-
-    private function deleteTable($tableName) {
-        if ($this->connect()) {
-            $query = "DROP table ".$tableName;
-
-            // Insert query to database
-            $result = mysqli_query($this->conn, $query);
-            echo "\nQUERY: ".$query;
-            echo "\nResult: ".$result;
-            $this->destroyConn();
-
-            if ($result) return TRUE;
-            else return FALSE;
-        }
-        else return FALSE;
-    }
-
-    private function insertData(){
-        foreach ($this->data as $row) {
-//            echo "\nrow dr insert: ";
-//            var_dump($row);
-            $res = $this->api_model->insert_table($_SESSION["tableName"],$row);
-//            echo "\nhasil insert: ".$res;
-        }
-    }
-
-    // Unused function
-    private function insertTable($tableValue) {
-        $query = 'INSERT INTO ' . $_SESSION["tableName"] . '(';
-
-        $numItems = count($this->columnName);
-        $i = 0;
-        foreach ($this->columnName as $col) {
-            $query = $query . $col;
-            if (++$i != $numItems) // not last index
-                $query = $query . ',';
-            else
-                $query = $query . ') VALUES ';
-        }
-
-        $numItems = count($tableValue);
-        $i = 0;
-        foreach ($tableValue as $row) {
-            $numItemsRow = count($row);
-            $j = 0;
-            foreach($row as $val) {
-                $query = $query . '(' . $val;
-                if (++$j != $numItemsRow) // not last index
-                    $query = $query . ',';
-                else
-                    $query = $query . ')';
-            }
-            if (++$i != $numItems) // not last index
-                $query = $query . ',';
-        }
-
-        // Insert query to database
-        $result = mysqli_query($this->conn, $query);
-
-        if ($result) return TRUE;
-        else return FALSE;
-    }
-
-    private function getRecords($query) {
-        $result = mysqli_query($this->conn, $query);
-
-        if (!$result) {
-            echo 'Could not run query: ' . mysql_error();
-            exit;
-        }
-
-        $row = mysql_fetch_row($result);
-        return $row;
     }
 
 }
--- application/models/Api_model.php
+++ application/models/Api_model.php
@@ -7,9 +7,18 @@
  * Time: 11:55 PM
  */
 class Api_model extends CI_Model {
+    // specify own database credentials
+    private $host = "localhost";
+    private $db_name = "extensibleVisualization";
+    private $username = "root";
+    private $conn;
+    private $password = "";
+    private $tabledb;
 
     public function __construct() {
         parent::__construct();
+        $this->tabledb = new TableDB;
+        $this->conn = null;
     }
 
     public function get_table($tableName) {
@@ -29,4 +38,185 @@ class Api_model extends CI_Model {
         return $query;
     }
 
+    private function connect(){
+        $this->conn = mysqli_connect($this->host, $this->username, $this->password, $this->db_name);
+        if (mysqli_connect_errno()){
+            // can't connect to mySQL
+        }
+        return $this->conn;
+    }
+
+    private function destroyConn(){
+        mysqli_close($this->conn);
+    }
+
+    public function setData($data) {
+        $this->tabledb->setData($data);
+    }
+
+    public function getData() {
+        return $this->tabledb->getData();
+    }
+
+    public function invokeCreateTable() {
+        $_SESSION["tableName"] = $this->generateString(50);
+        $this->tabledb->setColumnName(array_keys($this->tabledb->getData()[0]));
+        $dataExample = $this->tabledb->getData()[0];
+        $dataExample = array_values($dataExample);
+        return ($this->createTable($_SESSION["tableName"], $this->tabledb->getColumnName(), $dataExample));
+    }
+
+    private function generateString($length){
+        $charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+        $key = "";
+        for($i=0; $i<$length; $i++)
+            $key .= $charset[(mt_rand(0,(strlen($charset)-1)))];
+        return $key;
+    }
+
+    private function createTable($tableName, $columnName, $dataExample) {
+
+        if ($this->connect()) {
+            // Build mySQL query to create table
+            $query = 'CREATE TABLE IF NOT EXISTS `' . $tableName . '`(';
+            $numItems = count($columnName);
+            $i = 0;
+
+            // Dimension/Measure session preparation
+            $dimensionArr = array();
+            $measureArr = array();
+            $measureTypeArr = array();
+
+            foreach ($columnName as $col) {
+                if (ctype_digit($dataExample[$i])) {
+                    $query = $query . '`' . $col . '` INT NOT NULL';
+                    array_push($measureArr, $col);
+                    array_push($measureTypeArr, "SUM");
+                }
+                else {
+                    $query = $query . '`' . $col . '` TEXT NOT NULL';
+                    array_push($dimensionArr, $col);
+                }
+
+                $i++;
+                if ($i != $numItems) // not last index
+                    $query = $query . ',';
+                else
+                    $query = $query . ')';
+            }
+
+            // Push column name to Dimension/Measure session
+            $_SESSION["dimension"] = $dimensionArr;
+            $_SESSION["measure"] = $measureArr;
+
+            // Insert query to database
+            $result = mysqli_query($this->conn, $query);
+            echo "\nQUERY: ".$query;
+            echo "\nResult: ".$result;
+            $this->destroyConn();
+
+            if ($result) return TRUE;
+            else return FALSE;
+        }
+        else return FALSE;
+    }
+
+    public function deleteTable($tableName) {
+        if ($this->connect()) {
+            $query = "DROP table ".$tableName;
+
+            // Insert query to database
+            $result = mysqli_query($this->conn, $query);
+            echo "\nQUERY: ".$query;
+            echo "\nResult: ".$result;
+            $this->destroyConn();
+
+            if ($result) return TRUE;
+            else return FALSE;
+        }
+        else return FALSE;
+    }
+
+    public function insertData(){
+        foreach ($this->tabledb->getData() as $row) {
+//            echo "\nrow dr insert: ";
+//            var_dump($row);
+            $res = $this->insert_table($_SESSION["tableName"],$row);
+//            echo "\nhasil insert: ".$res;
+        }
+    }
+
+    // Unused function
+    private function insertTable($tableValue) {
+        $query = 'INSERT INTO ' . $_SESSION["tableName"] . '(';
+
+        $numItems = count($this->tabledb->getColumnName());
+        $i = 0;
+        foreach ($this->tabledb->getColumnName() as $col) {
+            $query = $query . $col;
+            if (++$i != $numItems) // not last index
+                $query = $query . ',';
+            else
+                $query = $query . ') VALUES ';
+        }
+
+        $numItems = count($tableValue);
+        $i = 0;
+        foreach ($tableValue as $row) {
+            $numItemsRow = count($row);
+            $j = 0;
+            foreach($row as $val) {
+                $query = $query . '(' . $val;
+                if (++$j != $numItemsRow) // not last index
+                    $query = $query . ',';
+                else
+                    $query = $query . ')';
+            }
+            if (++$i != $numItems) // not last index
+                $query = $query . ',';
+        }
+
+        // Insert query to database
+        $result = mysqli_query($this->conn, $query);
+
+        if ($result) return TRUE;
+        else return FALSE;
+    }
+
+    private function getRecords($query) {
+        $result = mysqli_query($this->conn, $query);
+
+        if (!$result) {
+            echo 'Could not run query: ' . mysql_error();
+            exit;
+        }
+
+        $row = mysql_fetch_row($result);
+        return $row;
+    }
+}
+
+class TableDB {
+    private $columnName;
+    private $data;
+
+    public function __construct() {
+        $this->columnName = array();
+    }
+
+    public function getColumnName() {
+        return $this->columnName;
+    }
+
+    public function setColumnName($colName) {
+        $this->columnName = $colName;
+    }
+
+    public function getData() {
+        return $this->data;
+    }
+
+    public function setData($data) {
+        $this->data = $data;
+    }
 }
